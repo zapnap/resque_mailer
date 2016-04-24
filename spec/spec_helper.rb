7a8fe969860f6182423280ea16226fa66556066e
Nick Plante <nap@zerosum.org>
2016-04-23 21:45:19 -0400

    include hash with indifferent access for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'action_mailer'
 require 'resque_mailer'
 require 'rspec/autorun'
+require 'active_support/hash_with_indifferent_access'
 
 Resque::Mailer.excluded_environments = []
 ActionMailer::Base.delivery_method = :test
