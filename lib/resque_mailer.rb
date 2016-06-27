427a29be2983fa8e5d396f0ff777a3c989d08956
Brian Kelly <brian@fetlife.com>
2016-06-27 12:38:30 -0400

    Fix perform method's call to Mailer#new

--- lib/resque_mailer.rb
+++ lib/resque_mailer.rb
@@ -45,7 +45,12 @@ module Resque
       def perform(action, serialized_args)
         begin
           args = ::Resque::Mailer.argument_serializer.deserialize(serialized_args)
-          message = self.send(:new, action, *args).message
+          message ||= begin
+            mailer_class_send = @mailer_class.send(:new)
+            mailer_class_send.process(action, *@args)
+            mailer_class_send.message
+          end
+
           if message.respond_to?(:deliver_now)
             message.deliver_now
           else
